ebf915c8bf17f13e4ec13e5e5c841fbb67191ed3
Eric Harney <eharney@redhat.com>
2016-04-14 14:49:37 -0400

    Set nfs_snapshot_support in cinder.conf
    The current work at [1] makes this feature
    opt-in only.
    
    [1] https://review.openstack.org/#/c/147186/20
    
    Change-Id: I5b3df4212251f08dd269963cc3821fd09728bcc4

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -68,6 +68,9 @@ function configure_cinder_nfs {
     iniset $CINDER_CONF nfs nas_secure_file_permissions \
         ${NFS_SECURE_FILE_PERMISSIONS}
 
+    # NFS snapshot support is currently opt-in only.
+    iniset $CINDER_CONF nfs nfs_snapshot_support True
+
     # Point Cinder to the Nova service correctly
     # Cinder's defaults don't match what devstack sets up.
     iniset $CINDER_CONF DEFAULT nova_catalog_info compute:nova:publicURL
